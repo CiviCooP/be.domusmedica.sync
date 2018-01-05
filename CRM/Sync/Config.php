f1ce2ce73a5b4d905f27dcec56366d81a98e6e2c
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2018-01-05 10:39:52 +0100

    Singeton to store the configuration.

---
+++ CRM/Sync/Config.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * Singleton to store the technical ids of the custom fields
+ *
+ * @author Klaas Eikelbooml (CiviCooP) <klaas.eikelboom@civicoop.org>
+ * @date 5-jan-2018
+ * @license AGPL-3.0
+ *
+ */
+
+class CRM_Sync_Config {
+
+  private static $_singleton;
+
+  private $_bankrekeningCustomFieldId;
+
+  private $_opleidingsjaarCustomFieldId;
+
+  private $_actiefvoorWachtdienstCustomFieldId;
+
+  private $_emdCustomFieldId;
+
+  /**
+   * CRM_Sync_Config constructor.
+   */
+  public function __construct() {
+    try {
+      $this->_bankrekeningCustomFieldId = civicrm_api3('CustomField', 'getvalue', array(
+        'return' => "id",
+        'name' => "Bankrekening",
+      ));
+    } catch (Exception $ex) {
+      throw new Exception('Oops: Custom Field Bankrekening not found in configuration (File ' . __FILE__ . ' on line ' . __LINE__ . ')');
+    }
+    try {
+      $this->_opleidingsjaarCustomFieldId = civicrm_api3('CustomField', 'getvalue', array(
+        'return' => "id",
+        'name' => "Opleidingsjaar",
+      ));
+    } catch (Exception $ex) {
+      throw new Exception('Oops: Custom Field Opleidingsjaar not found in configuration (File ' . __FILE__ . ' on line ' . __LINE__ . ')');
+    }
+    try {
+      $this->_actiefvoorWachtdienstCustomFieldId = civicrm_api3('CustomField', 'getvalue', array(
+        'return' => "id",
+        'name' => "Vrijgesteld_van_wacht2",
+      ));
+    } catch (Exception $ex) {
+      throw new Exception('Oops: Custom Field Vrijgesteld_van_wacht2 not found in configuration (File ' . __FILE__ . ' on line ' . __LINE__ . ')');
+    }
+    try {
+      $this->_emdCustomFieldId = civicrm_api3('CustomField', 'getvalue', array(
+        'return' => "id",
+        'name' => "Medisch_pakket2",
+      ));
+    } catch (Exception $ex) {
+      throw new Exception('Oops: Custom Field Medisch_pakket2 not found in configuration (File ' . __FILE__ . ' on line ' . __LINE__ . ')');
+    }
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getBankrekeningCustomFieldId() {
+    return $this->_bankrekeningCustomFieldId;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getOpleidingsjaarCustomFieldId() {
+    return $this->_opleidingsjaarCustomFieldId;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getActiefvoorWachtdienstCustomFieldId() {
+    return $this->_actiefvoorWachtdienstCustomFieldId;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getEmdCustomFieldId() {
+    return $this->_emdCustomFieldId;
+  }
+
+
+  /**
+   * Instantiates the singleton
+   *
+   * @return Sync_Config
+   * @access public
+   * @static
+   */
+  public static function singleton() {
+    if (!self::$_singleton) {
+      self::$_singleton = new CRM_Sync_Config();
+    }
+    return self::$_singleton;
+  }
+
+}
